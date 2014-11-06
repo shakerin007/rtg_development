52c0cd9d3421b1de244163c26cdb917fb292c485
angryanon <shakerin.ahmed@gmail.com>
2014-11-06 06:27:55 +0000

    added new folder named "test" to check separate files

---
+++ test/index.php
@@ -0,0 +1,25 @@
+<html>
+<body>
+<?php 
+$value1=$_POST["option"];
+if($value1 == "create_testbench"){
+    echo"<h2>$value1</h2>";
+echo "<form>
+ <p> Number of Environment in Test :<input type=\"number\" name=\"env_no\" min=\"1\" max=\"5\" value=\"1\"></br>
+Number of Virtual Sequencer in Test :<input type=\"number\" name=\"v_seq_no\" min=\"1\" max=\"5\" value=\"1\"></br>
+Number of Virtual Sequence in Test :<input type=\"number\" name=\"v_sqr_no\" min=\"1\" max=\"5\" value=\"1\"></br>
+Number of Agent per Environment in Test :<input type=\"number\" name=\"agent_no\" min=\"1\" max=\"5\" value=\"1\"></br>
+Number of Scoreboard per Environment in Test :<input type=\"number\" name=\"scb_no\" min=\"1\" max=\"5\" value=\"1\"></br>
+Number of Subscriber per Environment in Test :<input type=\"number\" name=\"sbr_no\" min=\"1\" max=\"5\" value=\"1\"></br>
+Number of Driver per Agent in Test :<input type=\"number\" name=\"drv_no\" min=\"1\" max=\"5\" value=\"1\"></br>
+Number of Monitor per Agent in Test :<input type=\"number\" name=\"mon_no\" min=\"1\" max=\"5\" value=\"1\"></br>
+Number of Sequencer per Agent in Test :<input type=\"number\" name=\"seq_no\" min=\"1\" max=\"5\" value=\"1\"></br>
+</p>
+</form>";
+}
+else
+    echo"my testbenches";
+    
+?>
+</body>    
+</html>
