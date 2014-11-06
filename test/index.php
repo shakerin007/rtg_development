938b12727d5657304235b3d2af080331984aeae2
angryanon <shakerin.ahmed@gmail.com>
2014-11-06 19:41:35 +0000

    html file generation from php is testes in edited files

--- test/index.php
+++ test/index.php
@@ -3,8 +3,8 @@
 <?php 
 $value1=$_POST["option"];
 if($value1 == "create_testbench"){
-    echo"<h2>$value1</h2>";
-echo "<form>
+/*    echo"<h2>$value1</h2>";
+echo "<form action=\"index1.php\" method=\"post\">
  <p> Number of Environment in Test :<input type=\"number\" name=\"env_no\" min=\"1\" max=\"5\" value=\"1\"></br>
 Number of Virtual Sequencer in Test :<input type=\"number\" name=\"v_seq_no\" min=\"1\" max=\"5\" value=\"1\"></br>
 Number of Virtual Sequence in Test :<input type=\"number\" name=\"v_sqr_no\" min=\"1\" max=\"5\" value=\"1\"></br>
@@ -14,12 +14,18 @@ Number of Subscriber per Environment in Test :<input type=\"number\" name=\"sbr_
 Number of Driver per Agent in Test :<input type=\"number\" name=\"drv_no\" min=\"1\" max=\"5\" value=\"1\"></br>
 Number of Monitor per Agent in Test :<input type=\"number\" name=\"mon_no\" min=\"1\" max=\"5\" value=\"1\"></br>
 Number of Sequencer per Agent in Test :<input type=\"number\" name=\"seq_no\" min=\"1\" max=\"5\" value=\"1\"></br>
-</p>
-</form>";
-}
-else
-    echo"my testbenches";
+<input type=\"submit\" name=\"Submit\" value=\"submit\"></p>
+
+</form>";*/
+include("Testbench_hierarchy.html");
+
     
+}
+
+else{
+    
+     include("Testbench_hierarchy.html");
+}
 ?>
 </body>    
 </html>
