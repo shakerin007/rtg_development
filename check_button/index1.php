2a031c390b8c2b726a4e72f33ea0de977eeaf9db
angryanon <shakerin.ahmed@gmail.com>
2014-11-19 17:55:41 +0000

    New folders are added. signin_test contains files that are examples of connecting mysql server with php

---
+++ check_button/index1.php
@@ -0,0 +1,28 @@
+<html>
+<body>
+<?php 
+$env_no=$_POST["env_no"];
+$v_seq_no=$_POST["v_seq_no"];
+$v_sqr_no=$_POST["v_sqr_no"];
+$agent_no=$_POST["agent_no"];
+$scb_no=$_POST["scb_no"];
+$sbr_no=$_POST["sbr_no"];
+$drv_no=$_POST["drv_no"];
+$mon_no=$_POST["mon_no"];
+$seq_no=$_POST["seq_no"];
+
+echo"<h2>Testbench Information Review</h2>";
+echo"<p>Number of Environment :$env_no </p>";
+echo"<p>Number of Virtual Sequence :$v_seq_no</p>";
+echo"<p>Number of Virtual Sequencer :$v_sqr_no</p>";
+echo"<p>Number of Agent per Environment :$agent_no</p>";
+echo"<p>Number of Scoreboard per env :$scb_no</p>";
+echo"<p>Number of Subscriber per env :$sbr_no</p>";
+echo"<p>Number of Driver per Agent :$drv_no</p>";
+echo"<p>Number of Monitor per Agent :$mon_no</p>";
+ echo"<p>Number of Sequencer per Agent :$seq_no</p>";
+
+ 
+?>
+</body>    
+</html>
